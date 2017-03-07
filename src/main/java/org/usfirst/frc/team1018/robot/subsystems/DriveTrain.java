72e8bd94965dbc087dd4b35d9b6e4c0ca1b69726
firecrafty <bluer2288@students.pike.k12.in.us>
2017-03-07 09:47:11 -0500

    Document DriveTrain.java

--- src/main/java/org/usfirst/frc/team1018/robot/subsystems/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team1018/robot/subsystems/DriveTrain.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1018.robot.subsystems;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team1018.robot.Robot;
 import org.usfirst.frc.team1018.robot.commands.Drive;
@@ -12,11 +13,21 @@ import org.usfirst.frc.team1018.robot.commands.Drive;
  *         Contains methods to control the robot's mecanum style drivetrain
  */
 public class DriveTrain extends Subsystem {
-
+    /**
+     * Drives the robot using the mecanum style drive train
+     */
     public void drive() {
         Robot.map.driveTrain.mecanumDrive_Cartesian(Robot.oi.leftStick.getX(), Robot.oi.leftStick.getY(), Robot.oi.rightStick.getX(), 0);
     }
 
+    /**
+     * Sets the default command to the <code>Drive</code> command
+     * <p>
+     * The default command of a subsystem is run continuously as long as there is not another command running that
+     * requires that subsystem.
+     *
+     * @see edu.wpi.first.wpilibj.command.Subsystem#setDefaultCommand(Command)
+     */
     public void initDefaultCommand() {
         setDefaultCommand(new Drive());
     }
