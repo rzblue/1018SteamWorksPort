20190da4edab66a71c18e9253b37261ff83cd0b3
firecrafty <bluer2288@students.pike.k12.in.us>
2017-04-05 17:32:40 -0400

    loader

--- vision_raspi/src/main/java/org/usfirst/frc/team1018/vision/utils/NativeLoader.java
+++ vision_raspi/src/main/java/org/usfirst/frc/team1018/vision/utils/NativeLoader.java
@@ -1,8 +1,13 @@
 package org.usfirst.frc.team1018.vision.utils;
 
 import cz.adamh.utils.NativeUtils;
+import org.usfirst.frc.team1018.vision.Main;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 /**
  * @author Ryan Blue
@@ -16,6 +21,25 @@ public class NativeLoader {
             NativeUtils.loadLibraryFromJar("/libopencv_java310.so");
         } catch(IOException e) {
             e.printStackTrace();
+        }
+    }
+    /*private static String getOpenCvPackageFromManifest() {
+        Manifest m = getManifest(Main.class);
+        Attributes s = m.getAttributes("Build-Type");
+        return getManifest(Main.class).getAttributes()
+    }*/
+    public static Manifest getManifest(Class<?> clz) {
+        String resource = "/" + clz.getName().replace(".", "/") + ".class";
+        String fullPath = clz.getResource(resource).toString();
+        String archivePath = fullPath.substring(0, fullPath.length() - resource.length());
+        if (archivePath.endsWith("\\WEB-INF\\classes") || archivePath.endsWith("/WEB-INF/classes")) {
+            archivePath = archivePath.substring(0, archivePath.length() - "/WEB-INF/classes".length()); // Required for wars
+        }
+
+        try (InputStream input = new URL(archivePath + "/META-INF/MANIFEST.MF").openStream()) {
+            return new Manifest(input);
+        } catch (Exception e) {
+            throw new RuntimeException("Loading MANIFEST for class " + clz + " failed!", e);
         }
     }
 
